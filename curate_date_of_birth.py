1fb3e915c1a7327d40218c843578cbd78690835b
James Farrell <33842132+JamesAFarrell@users.noreply.github.com>
2024-02-26 12:12:15 +0000

    Add hes_op_date_of_birth
    Add function `hes_op_date_of_birth` which extracts data of birth data from HES-OP

--- curate_date_of_birth.py
+++ curate_date_of_birth.py
@@ -47,3 +47,47 @@ def hes_apc_date_of_birth(hes_apc: DataFrame) -> DataFrame:
     )
 
     return date_of_birth_hes_apc
+
+
+def hes_op_date_of_birth(hes_op: DataFrame) -> DataFrame:
+    """
+    Process the date of birth data from the HES-OP (Outpatients) table, ensuring distinct records.
+
+    The 'apptage_calc' column in HES-OP represents the age of the individual at the time of appointment.
+    For those aged 1 year or more, these are in completed years, while those less than 1 year of age have
+    decimalised values. When converting age to date of birth, an upward adjustment of 0.5 years is applied
+    to those aged 1 year or more to approximate fractional ages.
+
+    Args:
+        hes_op (DataFrame): DataFrame containing the HES-OP table data.
+
+    Returns:
+        DataFrame: Processed DataFrame with metadata added.
+    """
+    date_of_birth_hes_op = (
+        hes_op
+        .select(
+            'person_id',
+            f.col('apptdate').alias('record_date'),
+            f.col('apptage_calc').alias('age_at_appointment')
+        )
+        .distinct()
+        .withColumn(
+            'date_of_birth',
+            f.when(
+                f.col('age_at_appointment') < f.lit(1),
+                f.date_sub(f.col('record_date'), f.round(f.col('age_at_appointment') * 365.25).cast('integer'))
+            )
+            .when(
+                f.col('age_at_appointment') >= f.lit(1),
+                f.date_sub(
+                    f.col('record_date'),
+                    f.round((f.col('age_at_appointment') + 0.5) * 365.25).cast('integer')
+                )
+            )
+        )
+        .drop('age_at_appointment')
+        .withColumn('data_source', f.lit('hes_op'))
+    )
+
+    return date_of_birth_hes_op
