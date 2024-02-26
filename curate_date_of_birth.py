a3b63a28557b2dc0539316fc06a14f9d2a2d6d84
James Farrell <33842132+JamesAFarrell@users.noreply.github.com>
2024-02-26 12:10:52 +0000

    Add hes_apc_date_of_birth
    Add function `hes_apc_date_of_birth` which extracts data of birth data from HES-APC

--- curate_date_of_birth.py
+++ curate_date_of_birth.py
@@ -23,3 +23,27 @@ def gdppr_date_of_birth(gdppr: DataFrame) -> DataFrame:
     )
 
     return date_of_birth_gdppr
+
+
+def hes_apc_date_of_birth(hes_apc: DataFrame) -> DataFrame:
+    """
+    Process the date of birth data from the HES-APC (Admitted Patient Care) table, ensuring distinct records.
+
+    Args:
+        hes_apc (DataFrame): DataFrame containing the HES-APC table data.
+
+    Returns:
+        DataFrame: Processed DataFrame with metadata added.
+    """
+    date_of_birth_hes_apc = (
+        hes_apc
+        .select(
+            'person_id',
+            f.col('epistart').alias('record_date'),
+            f.to_date('mydob', 'MMyyyy').alias('date_of_birth')
+        )
+        .distinct()
+        .withColumn('data_source', f.lit('hes_apc'))
+    )
+
+    return date_of_birth_hes_apc
