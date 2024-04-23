e6812d907a95f51605807d1478080ec7e96033b0
James Farrell <33842132+JamesAFarrell@users.noreply.github.com>
2024-04-23 10:27:32 +0100

    Refactor import statements and function names
    Updated import statements to explicitly import functions from respective modules.
    Added 'curate_sex' module

--- hds_functions/__init__.py
+++ hds_functions/__init__.py
@@ -1,7 +1,7 @@
-from .csv_utils import *
-from .curate_date_of_birth import *
-from .data_aggregation import *
-from .data_wrangling import *
-from .json_utils import *
-from .spark_session import *
-from .table_management import *
+from .csv_utils import read_csv_file, write_csv_file
+from .curate_date_of_birth import create_date_of_birth_multisource, create_date_of_birth_individual
+from .curate_sex import create_sex_multisource, create_sex_individual
+from .data_aggregation import first_row, 
+from .data_wrangling import melt, clean_column_names, map_column_values
+from .json_utils import read_json_file, write_json_file
+from .table_management import load_table, save_table
