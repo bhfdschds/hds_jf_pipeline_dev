a3c412ee7cde8af6b98d94d116e43a1f49a7ee51
James Farrell <33842132+JamesAFarrell@users.noreply.github.com>
2025-06-20 21:22:30 +0100

    refactor: expose `resolve_path`

--- hds_functions/__init__.py
+++ hds_functions/__init__.py
@@ -3,6 +3,6 @@ from .csv_utils import read_csv_file, write_csv_file, create_dict_from_csv
 from .data_aggregation import first_row
 from .data_privacy import round_counts_to_multiple, redact_low_counts
 from .data_wrangling import melt, clean_column_names, map_column_values
-from .environment_utils import find_project_folder
+from .environment_utils import find_project_folder, resolve_path
 from .json_utils import read_json_file, write_json_file
 from .table_management import load_table, save_table
